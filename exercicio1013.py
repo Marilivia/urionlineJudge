b2141dd8cf78c977ca973329d44c30e3cc780dfb
marilivia <mariliviamelo@hotmail.com>
2019-05-04 12:07:02 -0300

    exercicio iniciante

---
+++ exercicio1013.py
@@ -0,0 +1,25 @@
+"""
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+
+
+"""
+def maior(a,b,c):
+    maiorAB = ((a+b)+abs(a-b))/2
+    maiorAB = ((maiorAB+c)+abs(maiorAB-c))/2
+    return maiorAB
+n1 = input().split()
+x = int(n1[0])
+y = int(n1[1])
+z = int(n1[2])
+print(x,y,z)
+print(maior(x,y,z),'eh o maior')
